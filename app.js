41667656789c7b79989aa052c1b45bee89d511b0
Pioupiou325 <127089710+Pioupiou325@users.noreply.github.com>
2023-11-20 02:02:42 +0100

    app.use api books and auth to bottom in app.js to function

--- app.js
+++ app.js
@@ -4,29 +4,29 @@ const mongoose = require("mongoose");
 const bookRoutes = require("./routes/book");
 const userRoutes = require("./routes/user");
 
-app.use("/api/books", bookRoutes);
-app.use("/api/auth", userRoutes);
+app.use(express.json());
 mongoose
-  .connect(
-    `mongodb+srv://monvieuxgrimoire7:monvieuxgrimoire7@cluster0.staviob.mongodb.net/mon_vieux_grimoire?retryWrites=true&w=majority`,
-    { useNewUrlParser: true, useUnifiedTopology: true }
+.connect(
+  `mongodb+srv://testeur:testeur@cluster0.staviob.mongodb.net/mon_vieux_grimoire?retryWrites=true&w=majority`,
+  { useNewUrlParser: true, useUnifiedTopology: true }
   )
   .then(() => console.log("Connexion à MongoDB réussie !"))
   .catch(() => console.log("Connexion à MongoDB échouée !"));
-
-app.use(express.json());
-
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-  );
-  res.setHeader(
-    "Access-Control-Allow-Methods",
-    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-  );
-  next();
-});
-
+  
+  
+  app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+      );
+      res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+        );
+        next();
+      });
+      
+      app.use("/api/books", bookRoutes);
+      app.use("/api/auth", userRoutes);
 module.exports = app;
