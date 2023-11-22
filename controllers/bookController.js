e311e7205a0c2a1c2574ba4ad521be89eba404e8
Pioupiou325 <127089710+Pioupiou325@users.noreply.github.com>
2023-11-22 15:10:56 +0100

    fix rating(s) in model book

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -2,7 +2,6 @@ const Book = require("../models/Book");
 const fs = require("fs");
 
 exports.createBook = (req, res, next) => {
-  console.log("createBook");
   const bookObject = JSON.parse(req.body.book);
   delete bookObject._id;
   delete bookObject._userId;
@@ -31,34 +30,9 @@ exports.createBook = (req, res, next) => {
     });
 };
 
-exports.getOneBook = (req, res, next) => {
-  console.log("getOneBook");
-  Book.findOne({
-    _id: req.params.id,
-  })
-    .then((book) => {
-      console.log("getOneBook");
-      res.status(200).json(book);
-    })
-    .catch((error) => {
-      res.status(404).json({ error });
-    });
-};
 
-exports.getBestsBooks = (req, res, next) => {
-  const bestBooks = Book.find()
-    .sort({ averageRating: -1 })
-    .then((books) => {
-      const bestBooks = books.slice(0, 3);
-      res.status(200).json(bestBooks);
-    })
-    .catch((error) => {
-      console.log("erreur pour 3 bests averageRating");
-      res.status(400).json({
-        error: error,
-      });
-    });
-};
+
+
 
 exports.noteBook = (req, res, next) => {
   Book.find();
@@ -106,7 +80,7 @@ exports.deleteBook = (req, res, next) => {
       res.status(500).json({ error });
     });
 };
-
+////////////ok///////
 exports.getAllBook = (req, res, next) => {
   Book.find()
     .then((books) => {
@@ -114,5 +88,31 @@ exports.getAllBook = (req, res, next) => {
     })
     .catch((error) => {
       res.status(400).json({ error });
+    });
+};
+exports.getBestsBooks = (req, res, next) => {
+  Book.find()
+    .sort({ averageRating: -1 })
+    .then((books) => {
+      const bestBooks = books.slice(0, 3);
+      res.status(200).json(bestBooks);
+    })
+    .catch((error) => {
+      console.log("erreur pour 3 bests averageRating");
+      res.status(400).json({
+        error: error,
+      });
+    });
+};
+exports.getOneBook = (req, res, next) => {   
+  
+  Book.findOne({
+    _id: req.params.id,
+  })
+    .then((book) => {      
+      res.status(200).json(book);
+    })
+    .catch((error) => {
+      res.status(404).json({ error });
     });
 };
