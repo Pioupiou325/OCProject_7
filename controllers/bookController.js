2611c999fb5478b5123a09b884c161219582c095
Pioupiou325 <127089710+Pioupiou325@users.noreply.github.com>
2023-11-23 17:34:43 +0100

    modif return ratings

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -10,10 +10,9 @@ exports.createBook = (req, res, next) => {
     userId: req.auth.userId,
     imageUrl: `${req.protocol}://${req.get("host")}/images/${
       req.file.filename
-      }`,
+    }`,
     // ratings: [],
     // averageRating:0,
-
   });
 
   book
@@ -26,88 +25,9 @@ exports.createBook = (req, res, next) => {
     });
 };
 
-exports.noteBook = (req, res, next) => {
-  console.log(req.body);
-  const userId = req.auth.userId;
-  const note = req.body.rating;
-// on cherche le livre à noter
-  Book.findOne({ _id: req.params.id })
-    .then((book) => {
-      if (!book) {
-        // si le livre n existe pas 
-        return res.status(404).json({ message: "Livre non trouvé" });
-      }
 
-      // si il existe on continue 
-      // je recupere l id du créateur du livre
-      const creatorBook = book.userId;
-      // je le .some dans le tableau book.ratings pour savoir si il y est déjà
-      const userAlreadyRated = book.ratings.some(
-        (rating) => rating.userId === userId
-      );
-      console.log("userAlreadyRated est " + userAlreadyRated)
-      // si le user a déjà noté ou si c est le créateur du livre on return
-      if (userAlreadyRated || creatorBook === userId) {
-        return res
-          .status(403)
-          .json({
-            message: "Vous avez déjà noté ce livre ou vous êtes l'auteur",
-          });
-      }
-      // sinon on continue et on teste la note entre 0 et 5
-      if (note < 0 || note > 5) {
-        return res
-          .status(403)
-          .json({ message: "La note doit être comprise entre 0 et 5" });
-      }
-      // on continue pour la moyenne
-      
-      let newAverageNoteBook = 0;
-      let sommeRatings = 0;
-      // une boucle pour ajouter chaque note existante à sommeRatings
-      for (i = 0; i < book.ratings.length; i++) {
-        console.log(i);
-        sommeRatings += book.ratings[i].grade;
-      }
-      // on rajoute la note du req 
-      sommeRatings += note;
-      // on calcule la moyenne en ajoutant 1 à la longueur du tableau ratings
-      newAverageNoteBook = sommeRatings / (book.ratings.length + 1);
-      // console.log(newAverageNoteBook);
-      // console.log({ userId: userId, grade: note })
-      
-      // on push le nouveau rating et on modifie la moyenne
 
-      book.ratings.push({ userId: userId, grade: note });
-      book.averageRating = newAverageNoteBook;
-      // on save dans la base de donnée mongoDB
-      book.save()
-        .then((book) => res.status(200).json(book))
-        .catch((error) => {
-          res
-            .status(500)
-            .json({
-              message: "Erreur lors de la notation du livre",
-              error: error.message,
-            });
-        });
-      
-                
-
-    })
-    
-    .catch((error) => {
-      res
-        .status(500)
-        .json({
-          message: "Erreur lors de la notation du livre",
-          error: error.message,
-        });
-    });
-};
-
-exports.modifyBook = (req, res, next) => {
-  console.log("modifyBook");
+exports.modifyBook = (req, res, next) => {  
   const bookObject = req.file
     ? {
         ...JSON.parse(req.body.book),
@@ -119,7 +39,7 @@ exports.modifyBook = (req, res, next) => {
   delete bookObject._userId;
   Book.findOne({ _id: req.body.userId }).then((book) => {
     if (book.userId != req.auth.userId) {
-      res.statut(401).JSON({ message: "non autorisé" });
+      res.statut(403).JSON({ message: "unauthorized request" });
     } else {
       Book.updateOne({ _id: req.params.id }, { bookObject, _id: req.params.id })
         .then(res.status(200).json({ message: "livre modifié" }))
@@ -165,8 +85,7 @@ exports.getBestsBooks = (req, res, next) => {
       const bestBooks = books.slice(0, 3);
       res.status(200).json(bestBooks);
     })
-    .catch((error) => {
-      console.log("erreur pour 3 bests averageRating");
+    .catch((error) => {     
       res.status(400).json({
         error: error,
       });
@@ -181,5 +100,71 @@ exports.getOneBook = (req, res, next) => {
     })
     .catch((error) => {
       res.status(404).json({ error });
+    });
+};
+exports.noteBook = (req, res, next) => { 
+  const userId = req.auth.userId;
+  const note = req.body.rating;
+  // on cherche le livre à noter
+  Book.findOne({ _id: req.params.id })
+    .then((book) => {
+      if (!book) {
+        // si le livre n existe pas
+        return res.status(404).json({ message: "Livre non trouvé" });
+      }
+
+      // si il existe on continue
+      // je recupere l id du créateur du livre
+      const creatorBook = book.userId;
+      // je le .some dans le tableau book.ratings pour savoir si il y est déjà
+      const userAlreadyRated = book.ratings.some(
+        (rating) => rating.userId === userId
+      );     
+      // si le user a déjà noté ou si c est le créateur du livre on return
+      if (userAlreadyRated || creatorBook === userId) {
+        return res.status(403).json({
+          message: "Vous avez déjà noté ce livre ou vous êtes l'auteur",
+        });
+      }
+      // sinon on continue et on teste la note entre 0 et 5
+      if (note < 0 || note > 5) {
+        return res
+          .status(403)
+          .json({ message: "La note doit être comprise entre 0 et 5" });
+      }
+      // on continue pour la moyenne
+
+      let newAverageNoteBook = 0;
+      let sommeRatings = 0;
+      // une boucle pour ajouter chaque note existante à sommeRatings
+      for (i = 0; i < book.ratings.length; i++) {        
+        sommeRatings += book.ratings[i].grade;
+      }
+      // on rajoute la note du req
+      sommeRatings += note;
+      // on calcule la moyenne en ajoutant 1 à la longueur du tableau ratings
+      newAverageNoteBook = sommeRatings / (book.ratings.length + 1);
+      
+
+      // on push le nouveau rating et on modifie la moyenne
+
+      book.ratings.push({ userId: userId, grade: note });
+      book.averageRating = newAverageNoteBook;
+      // on save dans la base de donnée mongoDB
+      book
+        .save()
+        .then((book) => res.status(200).json(book))
+        .catch((error) => {
+          res.status(500).json({
+            message: "Erreur lors de la notation du livre",
+            error: error.message,
+          });
+        });
+    })
+    .catch((error) => {
+      res.status(500).json({
+        message: "Erreur lors de la notation du livre",
+        error: error.message,
+      });
     });
 };
