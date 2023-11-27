d4ff5b0aead8bc9aefc8aa640b69f34e9ebc00da
Pioupiou325 <127089710+Pioupiou325@users.noreply.github.com>
2023-11-27 13:59:01 +0100

    add webp to mimi types

---
+++ middleware/greenCodeImage.js
@@ -0,0 +1,10 @@
+const sharp = require('sharp');
+
+module.exports = (req, res, next) => {
+   try {
+       console.log(req.file.filename)
+	next();
+   } catch(error) {
+       res.status(401).json({ error });
+   }
+};
--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -4,13 +4,14 @@ const router = express.Router();
 const auth = require("../middleware/auth");
 const multer = require('../middleware/multer-config');
 const bookCtrl = require("../controllers/bookController");
+const greenCodeImage = require('../middleware/greenCodeImage');
 
 router.get("/bestrating", bookCtrl.getBestsBooks);
 router.get("/", bookCtrl.getAllBook);
 router.get("/:id", bookCtrl.getOneBook);
 router.post("/:id/rating", auth, bookCtrl.noteBook);
-router.put("/:id", auth, multer, bookCtrl.modifyBook);
-router.post("/", auth, multer, bookCtrl.createBook);
+router.put("/:id", auth, multer,greenCodeImage, bookCtrl.modifyBook);
+router.post("/", auth, multer,greenCodeImage, bookCtrl.createBook);
 router.delete("/:id", auth, bookCtrl.deleteBook);
 
 module.exports = router;
